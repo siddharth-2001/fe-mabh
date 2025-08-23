ee676abd1a7030638cdb398c49367ed66317f03b
siddharth-2001 <siddharthbharmoria@gmail.com>
2025-08-23 18:41:37 +0530

    change: puts blobs behind the card

--- src/components/home/Page1/Page1.tsx
+++ src/components/home/Page1/Page1.tsx
@@ -6,16 +6,23 @@ import styles from "./Page1.module.scss";
 export default function Page1() {
   return (
     <PageSection>
-      <div className={styles.mainContainer}>
-        <Tip text="Get ready to level up your digital presence" />
-        <div className={styles.typography}>
-          We Create Content that <span className={styles.accent}>Connects</span>{" "}
-          & <span className={styles.accent}>Converts</span>
+      <div className={styles.parentContainer}>
+        <div className={styles.animationContainer}>
+          <div className={`${styles.blob} ${styles.blob1}`}></div>
+          <div className={`${styles.blob} ${styles.blob2}`}></div>
+          <div className={styles.mainContainer}>
+            <Tip text="Get ready to level up your digital presence" />
+            <div className={styles.typography}>
+              We Create Content that{" "}
+              <span className={styles.accent}>Connects</span> &{" "}
+              <span className={styles.accent}>Converts</span>
+            </div>
+            <div className={styles.subtitle}>
+              Top-Tier Digital Experiences for High-Impact Brands
+            </div>
+            <Button text="Let's Talk" />
+          </div>
         </div>
-        <div className={styles.subtitle}>
-          Top-Tier Digital Experiences for High-Impact Brands
-        </div>
-        <Button text="Let's Talk" />
       </div>
     </PageSection>
   );
